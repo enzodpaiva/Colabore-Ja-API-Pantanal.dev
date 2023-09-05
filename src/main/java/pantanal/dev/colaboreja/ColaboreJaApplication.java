40a6afb20c26ac63b0128bd5ec8c333380518a43
Enzo Paiva <enzodouglaspaiva@gmail.com>
2023-09-05 17:30:36 -0300

    [feature-inicializacao-projeto] instalando projeto e primeiros arquivos

---
+++ src/main/java/pantanal/dev/colaboreja/ColaboreJaApplication.java
@@ -0,0 +1,13 @@
+package pantanal.dev.colaboreja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaboreJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColaboreJaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pantanal/dev/colaboreja/ColaboreJaApplicationTests.java
@@ -0,0 +1,13 @@
+package pantanal.dev.colaboreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaboreJaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
