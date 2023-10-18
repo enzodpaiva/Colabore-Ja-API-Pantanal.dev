f900b1434edb8045ec7f37301179a1c60472b0f6
Enzo Paiva <enzodouglaspaiva@gmail.com>
2023-10-18 13:21:26 -0300

    Save status

---
+++ src/main/java/pantanal/dev/colaboreja/repository/TokenRepository.java
@@ -0,0 +1,22 @@
+package pantanal.dev.colaboreja.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import pantanal.dev.colaboreja.model.Token;
+
+//import ch.qos.logback.core.subst.Token;
+
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    @Query(value = """
+      select t from Token t inner join User u\s
+      on t.user.id = u.id\s
+      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      """)
+    List<Token> findAllValidTokenByUser(Integer id);
+
+    Optional<Token> findByToken(String token);
+}
